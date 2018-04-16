a9bef9bac91d67e4989cffb4a97914b4ebce578f
Maxtoq <maxime.toquebiau@hotmail.fr>
2018-04-16 13:50:30 +0200

    MVC + Fenetre.java

---
+++ src/Vue/Fenetre.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Vue;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author maxim
+ */
+public class Fenetre extends JFrame {
+    
+    
+    public Fenetre() {
+        super("Gestion centre hospitalier");
+        
+    }
+}
